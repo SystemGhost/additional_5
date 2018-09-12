dffdc0397a7a3671ec386b34e36a72dfd2131bd6
SystemGhost <jackie07364538@gmail.com>
2018-09-12 22:10:10 +0300

    task: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	if(str.length % 2 == 1) return false;	
+		var stack = [];
+	var openBracket = [];
+	var closeBracket = [];
+	var sameValue = [];
+	var temp1;
+	var temp2;
+	var tempPop;
+	for(let i=0; i < bracketsConfig.length; i++) {
+		openBracket.push(bracketsConfig[i][0]);
+		closeBracket.push(bracketsConfig[i][1]);
+	}
+
+	for(var i=0; i < openBracket.length; i++) {
+		if(closeBracket.indexOf(openBracket[i]) != -1 ) {
+			sameValue.push(openBracket[i]);
+		}
+	}
+
+	for(var i=0; i < str.length; i++) {
+		if(openBracket.indexOf(str[i]) != -1) {
+			if(stack.length != 0) {
+				tempPop = stack.pop();
+				if(str[i] == tempPop && sameValue.indexOf(str[i]) != -1 ) continue;
+				else {
+					stack.push(tempPop);
+				}
+			} 
+			stack.push(str[i]);
+		}
+		else {
+			temp1 = closeBracket.indexOf(str[i]);
+			tempPop = stack.pop();
+			if(str[i] == tempPop) continue;
+			temp2 = openBracket.indexOf(tempPop);
+			if(temp1 != temp2) return false;
+		}
+		}
+		
+		if(stack.length == 0) return true;
+		
+		return false;
+
 }
